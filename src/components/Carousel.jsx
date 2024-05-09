64e3c993be5792b6a93ab65fb57a3a792df097be
Pradeepkumar.K <98541388+pradeepkumar28302@users.noreply.github.com>
2024-05-09 12:38:00 +0530

    done on the image carousel react app

--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -7,7 +7,35 @@ import ArrowForwardIosIcon from "@mui/icons-material/ArrowForwardIos";
 
 // implement the class below
 class Carousel extends Component {
-  
+  constructor(){
+    super();
+    this.state={
+        x:0
+    };
+}
+    right(){
+        this.setState({
+            x:this.state.x>=2?0:this.state.x+1
+        });
+    }
+
+    left(){
+        this.setState({
+            x:this.state.x<=0?2:this.state.x-1
+        });
+    }
+
+    render(){
+        return(
+            <div className="image-box">
+                <div className="left-arrow symbol" onClick={()=>this.left()}><ArrowBackIosIcon/></div>
+                <div id="head">{images[this.state.x].title}</div>
+                <img src={images[this.state.x].img} alt=""/>
+                <div id="text">{images[this.state.x].subtitle}</div>
+                <div className="right-arrow symbol" onClick={()=>this.right()}><ArrowForwardIosIcon/></div>
+            </div>
+        )
+    }
 }
 
 export default Carousel;
